02e3bdc1cba4f7c640a1fff817876bc14a64032d
Valiantsin Palkhouski <thisis.evesko@mail.ru>
2025-05-18 20:38:51 +0300

    Handle user join and leave events in UpdateHandler
    Added logic to process `MyChatMember` updates to handle user join and leave events in chats. Ensures proper creation of new chat entries or removal of inactive users, with corresponding logs for better tracking.

--- src/TelegramBot.ApiService/Infrastructure/Services/Handlers/UpdateHandler.cs
+++ src/TelegramBot.ApiService/Infrastructure/Services/Handlers/UpdateHandler.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 using TelegramBot.ApiService.Application.Common.Interfaces;
 
 namespace TelegramBot.ApiService.Infrastructure.Services.Handlers;
@@ -14,34 +15,48 @@ public class UpdateHandler(ILogger logger, IServiceScopeFactory scopeFactory)
     {
         using var scope = scopeFactory.CreateScope();
         var context = scope.ServiceProvider.GetRequiredService<IApplicationDbContext>();
-        
-        var chatId = update.Message?.Chat.Id;
-        Guard.Against.Null(chatId, nameof(chatId));
 
-        var chat = await context.Chats.FirstOrDefaultAsync(c => c.ChatId == chatId.Value, cancellationToken);
-        
-        if (chat == null)
+        if (update.Type is UpdateType.Message or UpdateType.MyChatMember)
         {
-            var entity = new TelegramBot.ApiService.Domain.Entities.Chat
+            var chatId = update.Message?.Chat.Id ?? update.MyChatMember?.Chat.Id;
+            Guard.Against.Null(chatId, nameof(chatId));
+
+            // Обработка входа пользователя в чат или его возвращения
+            if (update.MyChatMember?.NewChatMember.Status is ChatMemberStatus.Member or ChatMemberStatus.Administrator
+                or ChatMemberStatus.Creator)
             {
-                ChatId = chatId.Value,
-                Username = update.Message?.Chat.Username,
-                FirstName = update.Message?.Chat.FirstName,
-                LastName = update.Message?.Chat.LastName,
-                IsActive = true,
-                CreatedAt = DateTime.UtcNow,
-                LastInteractionAt = DateTime.UtcNow
-            };
-            
-            context.Chats.Add(entity);
-        }
-        else
-        {
-            chat.LastInteractionAt = DateTime.UtcNow;
-        }
+                var chat = await context.Chats.FirstOrDefaultAsync(c => c.ChatId == chatId.Value, cancellationToken);
+                
+                if (chat == null)
+                {
+                    var entity = new TelegramBot.ApiService.Domain.Entities.Chat
+                    {
+                        ChatId = chatId.Value,
+                        Username = update.MyChatMember.From.Username,
+                        FirstName = update.MyChatMember.From.FirstName,
+                        LastName = update.MyChatMember.From.LastName,
+                        IsActive = true,
+                        CreatedAt = DateTime.UtcNow,
+                        LastInteractionAt = DateTime.UtcNow
+                    };
+                    
+                    context.Chats.Add(entity);
+                    logger.LogInformation("User joined chat: {ChatId}", chatId);
+                }
+            }
+            // Обработка выхода пользователя из чата
+            else if (update.MyChatMember?.NewChatMember.Status is ChatMemberStatus.Left or ChatMemberStatus.Kicked)
+            {
+                var chat = await context.Chats.FirstOrDefaultAsync(c => c.ChatId == chatId.Value, cancellationToken);
+                
+                if (chat != null)
+                {
+                    context.Chats.Remove(chat);
+                    logger.LogInformation("User left chat: {ChatId}", chatId);
+                }
+            }
 
-        await context.SaveChangesAsync(cancellationToken);
-        logger.LogInformation("Get message with Text: {Text}", update.Message?.Text);
+            await context.SaveChangesAsync(cancellationToken);
+        }
     }
 }
-
